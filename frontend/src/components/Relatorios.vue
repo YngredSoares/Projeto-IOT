41b0723257ab3a09bbf9cf8c114e708c936bdd63
marcoscondejr <marcos14conde@hotmail.com>
2025-05-22 23:21:25 -0300

    feat: consumindo api do backend no frontend

---
+++ frontend/src/components/Relatorios.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+            <h1>Relatório</h1>
+            <ul>
+            <li v-for="rel in relatorio" :key="rel.id">
+                Sensor: {{ rel.sensor }} - Valor: {{ rel.valor }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                relatorio: []
+            }
+        },
+        mounted() {
+            fetch('/api/relatorio')
+                .then(response => {
+                    if (!response.ok) throw new Error('Erro ao buscar dados');
+                    return response.json();
+                })
+                .then(data => {
+                    this.relatorio = data;
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
+    }
+</script>
+
+<style>
+
+</style>
