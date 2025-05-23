19512b09a4b60e75b07554f2eb742698ee34cd96
marcoscondejr <marcos14conde@hotmail.com>
2025-05-22 22:35:36 -0300

    feat: criando a configuração do cors para a integração com o vue

--- backend/rotas/relatorioRouter.js
+++ backend/rotas/relatorioRouter.js
@@ -1,10 +1,8 @@
 const relatorioController = require('../controller/relatorioController');
-const usuarioController = require('../controller/usuarioController');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', relatorioController.criarRelatorio);
 router.get('/', relatorioController.exibirRelatorio);
-router.post('/', usuarioController.cadastrarUsuario);
 
 module.exports = router;
