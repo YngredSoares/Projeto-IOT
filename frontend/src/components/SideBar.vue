d3367160eef006eda3b68a9cebcb54974ea4a762
marcoscondejr <marcos14conde@hotmail.com>
2025-05-24 11:41:07 -0300

    feat: criação da sidebar e seus menus

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,18 +1,41 @@
 <template>
-    <div>
-        <nav class="bg-dark text-white vh-100 p-3" style="width: 250px;">
-            <h4>Menu</h4>
-            <ul class="nav flex-column">
-            <li class="nav-item mb-2">
-                <a href="#" class="nav-link text-white"><i class="bi bi-speedometer2"></i> Dashboard</a>
-            </li>
-            <li class="nav-item mb-2">
-                <a href="#" class="nav-link text-white"><i class="bi bi-file-bar-graph"></i> Relatórios</a>
-            </li>
-            <li class="nav-item mb-2">
-                <a href="#" class="nav-link text-white"><i class="bi bi-gear"></i> Configurações</a>
-            </li>
-            </ul>
+    <div class="sidebar">
+        <nav class="bg-dark text-white vh-100 p-3" style="width: 270px; align:">
+            <div class="d-flex flex-row gap-1">
+                <div class="d-flex user-info align-items-center p-2 gap-2">
+                    <img src="../assets/vue.svg" alt="Foto de perfil" style="width:40px;" class="rounded-pill"> 
+                </div>
+                <div class="d-flex flex-column justify-content-center">
+                    <h5 class="mb-0 text-white">Nome Usuário</h5>
+                    <a href="#" class="text-info small">Editar perfil</a>
+                </div>
+            </div>
+            <div class="menus">
+                <div class="p-3">
+                    <ul class="nav h5">
+                        <h4 class="mb-3 m-2 h5">Menu</h4>
+                    <li class="nav-item mb-3">
+                        <a href="#" class="nav-link text-white"><i class="bi bi-speedometer2"></i> Dashboard</a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <a href="#" class="nav-link text-white"><i class="bi bi-motherboard"></i> Aparelhos</a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <a href="#" class="nav-link text-white"><i class="bi bi-file-bar-graph"></i> Relatórios</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link text-white"><i class="bi bi-gear"></i> Configurações</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="rodape-nav">
+                    <ul class="nav h5 p-3">
+                        <li class="nav-item mb-5">
+                            <a href="#" class="nav-link text-white"><i class="bi bi-box-arrow-left"></i> Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </div>
 </template>
@@ -30,4 +53,22 @@ export default {
   border-radius: 4px;
   text-decoration: none;
 }
+
+.sidebar {
+    position: fixed;
+    left: 0;
+    top: 0;
+}
+
+.menus {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+a {
+    text-decoration: none;
+}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import BootstrapVue3 from 'bootstrap-vue-3'
 
 const app = createApp(App)
