da0c7e5b3aa61f9311a8990947cb2387ec99711f
marcoscondejr <marcos14conde@hotmail.com>
2025-05-25 00:29:00 -0300

    refactor: reajuste do app e tirando atribuindo reponsabilidades necessárias

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,25 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
-  import Dashboard from './components/Dashboard.vue'
   import SideBar from './components/SideBar.Vue'
-  import Controle from './components/Controle.vue'
 </script>
 
 <template>
-  <div class="layout d-flex justify-content-end me-1">
-    <!-- Sidebar -->
-    <SideBar />
-
-    <!-- Conteúdo principal -->
-    <main class="main-content">
-      <h2 class="text-start">Controle de Ar-Condicionado</h2>
-      <Controle/>
-      <div class="chart-container mt-3">
-        <div class="card-body">
-          <Dashboard />
-        </div>
-      </div>
-    </main>
+  <div class="layout">
+      <SideBar />
+      <main class="main-content">
+        <router-view />
+      </main>
   </div>
 </template>
 
 <style scoped>  
-  .main-content {
+.layout {
+  display: flex;
+  justify-content: end;
+}
+
+.main-content {
     width: 80%;
     display: flex;
     flex-direction: column;
-  }
-
-  .chart-container {
-    width: 100%;
-    height: 600px;
-  }
-
-  .card-body {
-    height: 100%;
-  }
+}
 </style>
