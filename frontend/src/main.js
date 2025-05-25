25500dd66124be3b6b852521816ddb78d5ef9e5f
marcoscondejr <marcos14conde@hotmail.com>
2025-05-25 00:27:21 -0300

    feat: adicionando o use das rotas no main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import BootstrapVue3 from 'bootstrap-vue-3'
+import router from './router';
 
 const app = createApp(App)
 
 app.use(BootstrapVue3)
+app.use(router);
 app.mount('#app')
