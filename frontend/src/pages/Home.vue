6ff365a4e42c03604f806496bfc72b45e1641d52
marcoscondejr <marcos14conde@hotmail.com>
2025-05-25 00:27:49 -0300

    feat: criando a page da home e seus imports

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import Dashboard from '../components/Dashboard.vue';
+  import Controle from '../components/Controle.vue'
+
+</script>
+
+<template>
+    <!-- Conteúdo principal -->
+    <main class="main-home me-2">
+      <h2 class="text-start">Controle de Ar-Condicionado</h2>
+      <Controle/>
+      <div class="chart-container mt-3">
+        <div class="card-body">
+          <Dashboard />
+        </div>
+      </div>
+    </main>
+</template>
+
+<style scoped>  
+
+</style>
