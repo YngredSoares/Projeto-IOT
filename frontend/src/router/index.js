e722c8fc8593ab2a59265c4063dc6c99f34dcad6
marcoscondejr <marcos14conde@hotmail.com>
2025-06-28 02:06:09 -0300

    wip

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Aparelho from '../pages/Aparelho.vue'
 import CadastrarAparelho from '../pages/CadastrarAparelho.vue'
+import Login from '../pages/Login.vue'
 
 const routes = [
+    { path: '/login', name: 'Login', component: Login},
     { path: '/', name: 'Home', component: Home},
     { path: '/aparelhos', name: 'Aparelho', component: Aparelho},
     { path: '/aparelhos/cadastrar', name: 'CadastrarAparelho', component: CadastrarAparelho},
