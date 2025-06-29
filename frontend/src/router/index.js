ff7bc73776e505f6a261acb87bf7f9650c5aae54
marcoscondejr <marcos14conde@hotmail.com>
2025-06-29 00:50:44 -0300

    feat: aplicando a validação de rotas para entrar somente quando estiver autenticado

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import store from '../store/modules/auth'
 import Home from '../pages/Home.vue'
 import Aparelho from '../pages/Aparelho.vue'
 import CadastrarAparelho from '../pages/CadastrarAparelho.vue'
@@ -7,16 +8,32 @@ import Login from '../pages/Login.vue'
 import CadastroUsuario from '../pages/CadastroUsuario.vue'
 
 const routes = [
-    { path: '/', name: 'Home', component: Home},
-    { path: '/login', name: 'Login', component: Login},
-    { path: '/cadastro', name: 'CadastroUsuario', component: CadastroUsuario},
-    { path: '/aparelhos', name: 'Aparelho', component: Aparelho},
-    { path: '/aparelhos/cadastrar', name: 'CadastrarAparelho', component: CadastrarAparelho},
+    { path: '/', redirect: '/login'},
+    { path: '/home', name: 'Home', component: Home, meta: { requiresAuth: true }},
+    { path: '/login', name: 'Login', component: Login, meta: { guest: true }},
+    { path: '/cadastro', name: 'CadastroUsuario', component: CadastroUsuario, meta: { guest: true }},
+    { path: '/aparelhos', name: 'Aparelho', component: Aparelho, meta: { requiresAuth: true }},
+    { path: '/aparelhos/cadastrar', name: 'CadastrarAparelho', component: CadastrarAparelho, meta: { requiresAuth: true }},
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to,from,next)=>{
+  const isAuthenticated = store.getters['auth/isAuthenticated']
+  console.log("Auth status:", isAuthenticated)
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } 
+  else if (to.meta.guest && isAuthenticated) {
+    next('/home')
+  } 
+  else {
+    next()
+  }
 })
 
 export default router
